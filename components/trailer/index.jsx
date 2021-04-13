388dffff03f053e73d3f6a8323ba89f9ab611459
Eugene Datsiuk <evgenii.datsiuk@gmail.com>
2021-04-13 06:54:32 +0300

    Updated trailer component: added styles to separate file.

---
+++ components/trailer/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Linking, Text, TouchableOpacity} from 'react-native';
+import {AntDesign} from '@expo/vector-icons';
+import {styles} from './style'
+
+const { button, text } = styles;
+
+export default function Index({video}) {
+  return (
+    <TouchableOpacity
+      style={button}
+      onPress={() => {
+        Linking.openURL(`https://youtu.be/${video.key}`);
+      }}
+    >
+      <AntDesign name="youtube" size={24} color="white" />
+      <Text
+        style={text}
+      >
+        {video.name}
+      </Text>
+    </TouchableOpacity>
+  );
+}
---
+++ components/trailer/style.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+        width: '100%',
+        minHeight: 60,
+        backgroundColor: 'red',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        borderRadius: 20,
+        marginVertical: 10,
+    },
+    text: {
+        fontFamily: 'Poppins-Regular',
+        fontSize: 16,
+        color: '#fff',
+        marginHorizontal: 20,
+    }
+});
